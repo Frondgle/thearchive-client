4c746df9ed80a27f76f2bfcbecef552574489d12
Eric Frey <ericlfrey@gmail.com>
2024-01-13 10:11:27 -0600

    Prepared Template

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 // import { useUser } from "@clerk/nextjs"; TODO: comment this line in to use Clerk Auth 
 import { useRouter } from "next/router";
 
@@ -8,17 +7,6 @@ export default function Home() {
   return (
     <>
       <h1 className="mb-3">Home Page</h1>
-      {/* {user?.id ? (
-        <h3>Welcome {user.username}, you are currently logged in!</h3>
-      ) : (
-        <>
-          <p>This is the main page. You can see all non-protected routes even if you aren't logged in!</p><p>
-            <a href="#" onClick={() => router.push('/restricted')} className="text-white-50">
-              Try to go to this Restricted Page while You are logged out.
-            </a>
-          </p>
-        </>
-      )} */}
     </>
   )
 }
