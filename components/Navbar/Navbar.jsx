e4ee04acd4dfa842213a824cbd3063352bff4d71
Angie Gonzalez <angiem.go@gmail.com>
2024-01-19 15:54:02 -0600

    added Prettier Husky and ESLint config

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -8,15 +8,7 @@ function NavBar() {
         <Link className="navbar-brand" passHref href="/">
           Stay Gold, Cowboy 🤠
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
