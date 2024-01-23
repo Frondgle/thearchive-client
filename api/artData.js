764e963b6c083e6b1b2c4e5fb55158ebf8ff47b1
Eric Frey <ericlfrey@gmail.com>
2024-01-23 17:58:55 -0600

    "Cleaned up the API page."

--- api/artData.js
+++ api/artData.js
@@ -1,5 +1,7 @@
+const dbURL = process.env.NEXT_PUBLIC_HEROKU_URL;
+
 const getArt = () => new Promise((resolve, reject) => {
-  fetch("https://the-sonatore-archive-840804772ccc.herokuapp.com/art", {
+  fetch(`${dbURL}/art`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -16,20 +18,8 @@ const getArt = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-// const getSingleArt = (id) => new Promise((resolve, reject) => {
-//   fetch(`https://the-sonatore-archive-840804772ccc.herokuapp.com/art/${id}`, {
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   })
-//     .then((response) => response.json())
-//     .then((data) => resolve(data))
-//     .catch(reject);
-// });
-
 const getSingleArt = (id) => new Promise((resolve, reject) => {
-  fetch(`https://the-sonatore-archive-840804772ccc.herokuapp.com/art/${id}`, {
+  fetch(`${dbURL}/art/${id}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
