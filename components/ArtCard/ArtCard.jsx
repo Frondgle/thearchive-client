bbd6c29987f61aef52f93c70fdc3561090a6e165
Eric Frey <ericlfrey@gmail.com>
2024-01-23 11:32:43 -0600

    "WIP. Basic Art Card Component. Nothing Fancy. Still need to style a bit more. Moving on to the Art Details Page and component."

--- components/ArtCard/ArtCard.jsx
+++ components/ArtCard/ArtCard.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import styles from './artcard.module.css';
 
 export default function ArtCard({ artObj }) {
-  const cloudinaryURL = 'https://res.cloudinary.com/dsakdzjkk/';
+  const cloudinaryURL = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
   return (
-    <Card className={styles.artCard} style={{ width: '12rem' }}>
-      <Card.Body className="p-0">
-        <Card.Title className={`art-card-title ${styles.cardTitle}`}>
-          {artObj.title || 'Untitled'}
-        </Card.Title>
-        {/* <Card.Text>{artObj.description || 'No Description'}</Card.Text> */}
-        {/* <Button variant="primary" className="btn-sm">
-          View Art
-        </Button> */}
-      </Card.Body>
-      <Card.Img
-        className={styles.cardImg}
-        src={`${cloudinaryURL}${artObj.pic}`}
-      />
-    </Card>
+    <Image
+      src={`${cloudinaryURL}${artObj.pic}`}
+      width={300}
+      height={300}
+      className={`${styles.cardImg}`}
+      alt="art card"
+    />
   );
 }
