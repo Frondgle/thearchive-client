aaaca52f4d26fbb84df2697c827c1c646d07593f
Eric Frey <ericlfrey@gmail.com>
2024-01-27 09:06:14 -0600

    Added the Head element and title to the ViewArtPage

--- pages/art/[id].jsx
+++ pages/art/[id].jsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { Image, Pagination } from 'react-bootstrap';
 import styles from './ViewArtPage.module.css';
+import Head from 'next/head';
 
 export default function ViewArtPage() {
   const [artObj, setArtObj] = useState({});
@@ -23,67 +24,53 @@ export default function ViewArtPage() {
 
   // Calculate previous and next IDs
   const prevId = currentIndex > 0 ? artIDs[currentIndex - 1] : null;
-  const nextId =
-    currentIndex < artIDs.length - 1 ? artIDs[currentIndex + 1] : null;
+  const nextId = currentIndex < artIDs.length - 1 ? artIDs[currentIndex + 1] : null;
 
   // Function to handle pagination
-  const handlePagination = pageId => {
+  const handlePagination = (pageId) => {
     if (pageId !== null) router.push(`/art/${pageId}`);
   };
 
   return (
-    <div className="d-flex flex-column">
-      <Pagination className="d-flex justify-content-center">
-        {prevId ? (
-          <Pagination.Prev onClick={() => handlePagination(prevId)} />
-        ) : (
-          <Pagination.Prev disabled />
-        )}
+    <>
+      <Head>
+        <title>{`The Sonatore Archive | ${artObj.title || 'Untitled'}`}</title>
+      </Head>
+      <div className="d-flex flex-column">
+        <Pagination className="d-flex justify-content-center">
+          {prevId ? <Pagination.Prev onClick={() => handlePagination(prevId)} /> : <Pagination.Prev disabled />}
 
-        {nextId ? (
-          <Pagination.Next onClick={() => handlePagination(nextId)} />
-        ) : (
-          <Pagination.Next disabled />
-        )}
-      </Pagination>
-      <div className="d-flex">
-        <Image
-          src={imageUrl}
-          alt={artObj.description}
-          className={styles.artImage}
-        />
+          {nextId ? <Pagination.Next onClick={() => handlePagination(nextId)} /> : <Pagination.Next disabled />}
+        </Pagination>
+        <div className="d-flex">
+          <Image src={imageUrl} alt={artObj.description} className={styles.artImage} />
 
-        <div
-          className={`${styles.detailsWrap} d-flex flex-column justify-content-between`}
-        >
-          <div>
-            <h5>
-              <i>{artObj.title || 'Untitled'} - </i>
-              {artObj.date_created
-                ? new Date(artObj.date_created).getFullYear()
-                : 'Undated'}
-            </h5>
-            <hr />
-            <p>- {artObj.description}</p>
-            <hr />
-            <p className={styles.info}>
-              {artObj.style} | {artObj.location || 'Location unknown'} |{' '}
-              {artObj.color ? 'Color' : 'Black & White'}
-            </p>
-            <hr />
-          </div>
-          <div>
-            <p className={styles.info}>
-              {artObj.frame_type} Frame | {artObj.film_type}
-            </p>
-            <hr />
-            <p className={styles.info}>
-              {artObj.mods ? 'Modified' : 'Unmodified'} |{' '}
-              {artObj.malfunction ? 'Malfunction' : 'No Malfunction'}
-            </p>
+          <div className={`${styles.detailsWrap} d-flex flex-column justify-content-between`}>
+            <div>
+              <h5>
+                <i>{artObj.title || 'Untitled'} - </i>
+                {artObj.date_created ? new Date(artObj.date_created).getFullYear() : 'Undated'}
+              </h5>
+              <hr />
+              <p>- {artObj.description}</p>
+              <hr />
+              <p className={styles.info}>
+                {artObj.style} | {artObj.location || 'Location unknown'} | {artObj.color ? 'Color' : 'Black & White'}
+              </p>
+              <hr />
+            </div>
+            <div>
+              <p className={styles.info}>
+                {artObj.frame_type} Frame | {artObj.film_type}
+              </p>
+              <hr />
+              <p className={styles.info}>
+                {artObj.mods ? 'Modified' : 'Unmodified'} | {artObj.malfunction ? 'Malfunction' : 'No Malfunction'}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
