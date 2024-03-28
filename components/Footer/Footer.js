bcd510f59128809c58debf4c16ddb463034f8d9f
Eric Frey <ericlfrey@gmail.com>
2024-03-28 18:10:38 -0500

    Changed the verbiage slightly.

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
-    <div className={styles.salmon}>Copyright 2024 Frondgle Solutions</div>
+    <div className={styles.salmon}>©️ {year} Frondgle Solutions</div>
   )
 }
