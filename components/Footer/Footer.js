02de125c9ceba0bb001e469df3e1c325ca82bf61
Eric Frey <ericlfrey@gmail.com>
2024-03-28 17:53:52 -0500

    Created a Footer component and corresponding CSS file with boiler plate code just to ensure it was showing on the page. Imported component to the _app.js file to show in all views.

---
+++ components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={styles.salmon}>Copyright 2024 Frondgle Solutions</div>
+  )
+}
