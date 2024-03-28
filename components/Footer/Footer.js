2c31f15a26a38a0fcb7e1b74c9b5ad618556d202
Eric Frey <ericlfrey@gmail.com>
2024-03-28 18:11:21 -0500

    renamed the class.

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,6 +4,6 @@ import styles from './Footer.module.css';
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <div className={styles.salmon}>©️ {year} Frondgle Solutions</div>
+    <div className={styles.footer}>©️ {year} Frondgle Solutions</div>
   )
 }
