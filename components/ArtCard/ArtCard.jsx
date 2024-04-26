e25667ad760c0d9e1134f141a3e96f6202693c7d
Eric Frey <ericlfrey@gmail.com>
2024-04-25 19:05:56 -0500

    jk being silly

--- components/ArtCard/ArtCard.jsx
+++ components/ArtCard/ArtCard.jsx
@@ -7,7 +7,7 @@ export default function ArtCard({ artObj }) {
   const router = useRouter();
   const cloudinaryURL = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
   return (
-    <div className={styles.cardWrap} onClick={() => router.push(`/art/${artObj.code}`)}>
+    <div className={styles.cardWrap} onClick={() => router.push(`/art/${artObj.id}`)}>
       <Image src={`${cloudinaryURL}${artObj.pic}`} alt="art card" className={`${styles.cardImg}`} height={300} width={300} />
       <div className={styles.captionWrap}>
         <div className={styles.caption}>
