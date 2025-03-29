93ea7bbf6ca427a08aa2b8a72bda39cb8e1f5f1a
Charles Bridgers IV (he/him) <charlesbridgersiv@gmail.com>
2025-03-29 14:40:11 -0500

    renamed navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
       <div className="container-fluid">
         <Link className={`navbar-brand ${styles.navbarBrand}`} passHref href="/">
           <Image src="/images/hat.png" alt="Golden Cowboy Hat" className={styles.logo} />
-          <span className={`ms-3 ${styles.title}`}>The Sonatore Archive 4EVER</span>
+          <span className={`ms-3 ${styles.title}`}>The Sonatore Archive</span>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
