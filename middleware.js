973f11f3ef61a27c54c68e344f92c967d61a0735
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2025-05-01 17:00:25 -0500

    commented out clerk should fix 500 error let's see

--- middleware.js
+++ middleware.js
@@ -1,12 +1,13 @@
-import { authMiddleware } from '@clerk/nextjs';
+// import { authMiddleware } from '@clerk/nextjs';
 
 // This example protects all routes except "/" including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: ['/', '/art/([0-9]+)', '/test']
-});
 
-export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+// export default authMiddleware({
+//   publicRoutes: ['/', '/art/([0-9]+)', '/test']
+// });
+
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
