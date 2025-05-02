7b3f47dfcd8f27e5f1d477cc9e776cd9b5fccc54
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2025-05-02 00:10:39 -0500

    removed clerk from package.json

--- middleware.js
+++ middleware.js
@@ -1,13 +1,3 @@
-// import { authMiddleware } from '@clerk/nextjs';
-
-// This example protects all routes except "/" including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-
-// export default authMiddleware({
-//   publicRoutes: ['/', '/art/([0-9]+)', '/test']
-// });
-
-// export const config = {
-//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
+export default function middleware(req) {
+  return NextResponse.next(); // Allow all requests to proceed
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import '@/styles/globals.css'
-// import { ClerkProvider } from '@clerk/nextjs' TODO: comment in to add Clerk Auth
 import '@/styles/globals.css';
 import NavBar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
-    // <ClerkProvider> // TODO: comment in to add Clerk Auth
     <>
       <NavBar />
       <div className="container">
@@ -14,6 +12,5 @@ export default function App({ Component, pageProps }) {
       </div>
       <Footer />
     </>
-    // </ClerkProvider> // TODO: comment in to add Clerk Auth
   );
 }
