516c01c471cde14d0dcd20024639d77861a63253
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2025-07-27 14:18:25 -0500

    created landing page, shit ton of mobile/design adjustments

--- components/RandomArtCard/RandomArtCard.jsx
+++ components/RandomArtCard/RandomArtCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import styles from './artcard.module.css';
+import styles from './randomartcard.module.css';
 import Image from 'next/image';
 
-export default function ArtCard({ artObj }) {
+export default function RandomArtCard({ artObj }) {
   const router = useRouter();
   const cloudinaryURL = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
   const optimizedImageUrl = `${cloudinaryURL}image/upload/w_300,h_363,q_auto:eco,f_auto,fl_progressive,fl_png8/${artObj.pic.split('image/upload/')[1]}`;
   
   return (
-    <div className={styles.cardWrap} onClick={() => router.push(`/art/${artObj.id}`)}>
+    <div className={styles.cardWrap} onClick={() => router.push('/photoGallery/photoGallery')}>
       <Image
         src={optimizedImageUrl}
         alt="art card"
@@ -19,11 +19,7 @@ export default function ArtCard({ artObj }) {
       />
       <div className={styles.captionWrap}>
         <div className={styles.caption}>
-          <p>{artObj.code || ''}</p>
-          <h5>
-            <i>{artObj.title || ''}</i>
-          </h5>
-          <p className={styles.description}>{artObj.description}</p>
+          <p className={styles.description}>Photo Gallery</p>
         </div>
       </div>
     </div>
