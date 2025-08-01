5fb6f1060ff9080646196332d1980b2906d721a5
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2025-08-01 12:27:15 -0500

    almost done but testing a design change

--- pages/art/[id].jsx
+++ pages/art/[id].jsx
@@ -1,12 +1,14 @@
 import { getArtIDs, getSingleArt } from '../../api/artData';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from './ViewArtPage.module.css';
 import Head from 'next/head';
+import { usePagination } from '../../context/PaginationContext';
 
 export default function ViewArtPage() {
   const [artObj, setArtObj] = useState({});
   const [artIDs, setArtIDs] = useState([]);
+  const { currentPage, setCurrentPage } = usePagination();
   const router = useRouter();
   const { id } = router.query;
   const cloudinaryURL = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
@@ -23,6 +25,29 @@ export default function ViewArtPage() {
   const currentIndex = artIDs.indexOf(Number(id));
   const prevId = currentIndex > 0 ? artIDs[currentIndex - 1] : null;
   const nextId = currentIndex < artIDs.length - 1 ? artIDs[currentIndex + 1] : null;
+
+  // useRef stores first pic user viewed in the gallery
+  const originalIDRef = useRef(null);
+
+  // Update the originalIDRef only once when currentIndex is first calculated
+  useEffect(() => {
+    if (originalIDRef.current === null && currentIndex >= 0) {
+      originalIDRef.current = currentIndex + 1;
+    }
+  }, [currentIndex]);
+
+  const originalID = originalIDRef.current; // first pic user viewed in the gallery
+  const manageCPID = currentIndex + 1; // current pic user is viewing
+  const remainder = manageCPID % 6;
+
+  useEffect(() => {
+    if (remainder === 0 && manageCPID < originalID) {
+      setCurrentPage(currentPage - 1);
+    }
+    if (remainder === 1 && manageCPID > originalID) {
+      setCurrentPage(currentPage + 1);
+    }
+  }, [manageCPID, remainder]);
 
   const handlePagination = (pageId) => {
     if (pageId !== null) router.push(`/art/${pageId}`);
