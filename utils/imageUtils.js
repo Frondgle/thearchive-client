7691e9ae7275f005e43bf68aca8bc7f4eb95c871
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2026-02-02 14:56:36 -0600

    improved photo quality

--- utils/imageUtils.js
+++ utils/imageUtils.js
@@ -1,5 +1,7 @@
 export const getOptimizedImageUrl = (picUrl) => {
     const cloudinaryURL = process.env.NEXT_PUBLIC_CLOUDINARY_URL;
     const imagePath = picUrl.split('image/upload/')[1];
-    return `${cloudinaryURL}image/upload/w_300,h_363,q_auto:eco,f_auto,fl_progressive,fl_png8/${imagePath}`;
+    // return `${cloudinaryURL}image/upload/w_300,h_363,q_auto:eco,f_auto,fl_progressive,fl_png8/${imagePath}`;
+    return `${cloudinaryURL}image/upload/w_1000,h_1210,q_auto:good,f_auto,fl_progressive/${imagePath}`;
+    // return `${cloudinaryURL}image/upload/w_auto,c_limit,dpr_auto,q_auto:good,f_auto,fl_progressive/${imagePath}`;
 };
